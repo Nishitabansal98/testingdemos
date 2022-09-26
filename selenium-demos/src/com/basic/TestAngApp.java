138bfea6a977fcbdd2fd3604e51d271fc77f6b48
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-27 01:05:46 +0530

    changes

--- selenium-demos/src/com/basic/TestAngApp.java
+++ selenium-demos/src/com/basic/TestAngApp.java
@@ -20,7 +20,6 @@ public class TestAngApp {
 		addProductToShoppingCart(webdriver, "ecomm", "headphones", "2");
 
 //		webdriver.quit();
-
 	}
 	private static void addProductToShoppingCart(WebDriver webdriver, String route, String productname, String quantity) {
 		webdriver.get(url + "/" + route);
