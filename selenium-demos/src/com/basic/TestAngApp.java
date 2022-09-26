f165f093dd90f7506b2cf36048fb8ad238e17acf
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-27 01:06:12 +0530

    changes

--- selenium-demos/src/com/basic/TestAngApp.java
+++ selenium-demos/src/com/basic/TestAngApp.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestAngApp {
-
 	private static String url = "http://localhost:4200";
 
 	public static void main(String[] args) {
