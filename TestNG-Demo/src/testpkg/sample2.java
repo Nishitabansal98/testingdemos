10729d1cd4a806a9706de0408297d3e9d30a0696
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-28 01:19:50 +0530

    changes

--- TestNG-Demo/src/testpkg/sample2.java
+++ TestNG-Demo/src/testpkg/sample2.java
@@ -1,5 +1,11 @@
 package testpkg;
 
-public class sample2 {
+import org.testng.annotations.Test;
 
-}
+public class sample2 {
+	 @Test
+		public  void demo()
+		{
+			System.out.println("demo2 running");
+		}
+	}
